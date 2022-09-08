066d585e9dc7909c2cf24b6e4e9b3fde44ed2a93
Thabo-Olaniyan <olaniyanthabo@gmail.com>
2022-09-08 16:48:12 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main- Prints a text
+ * main - Entry point
  *
- *  Return: Always 0 (success)
-/*
+ * Return: Always 0 (Success)
+ */
 {
-pritf("\"with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	pritf("\"with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 } 
