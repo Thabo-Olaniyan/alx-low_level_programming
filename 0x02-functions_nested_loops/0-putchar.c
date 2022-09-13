c5ddda47b915b2781ac44c8ede762bf6f462ebb5
Thabo-Olaniyan <olaniyanthabo@gmail.com>
2022-09-13 11:59:38 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,43 @@
 #include "main.h"
 
+
+
 /**
+
  * main - print putchar
- *
- * Return: Always 0
+
+ * Return: always 0
+
  */
 
+
+
 int main(void)
+
 {
+
 	_putchar('_');
+
 	_putchar('p');
+
 	_putchar('u');
+
 	_putchar('t');
+
 	_putchar('c');
+
 	_putchar('h');
+
 	_putchar('a');
-	_putchar('_');
+
+	_putchar('r');
+
 	_putchar('\n');
-										return (0);
+
+
+
+	return (0);
+
+
+
+}
